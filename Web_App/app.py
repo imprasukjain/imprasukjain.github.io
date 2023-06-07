ca5f60f6723966b528149232041aa125ebecc426
Prasuk Jain <78204285+imprasukjain@users.noreply.github.com>
2023-06-08 02:00:52 +0530

    Update app.py

--- Web_App/app.py
+++ Web_App/app.py
@@ -1,12 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for,flash,redirect
+from forms import RegistrationForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0'
 
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html',title='Home')
+
+@app.route('/error')
+def error():
+    return render_template('Error.html',title='Error')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for('hello_world'))
+    return render_template('forms.html',title='Register',form=form)
 
 
 
